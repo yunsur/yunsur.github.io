16e54c5fe6de6143430bccb0f2f6b4d7650bc87f
yunsur <yunsur.shi@gmail.com>
2018-05-02 06:19:04 +0800

    update stable 0502

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
-source "https://rubygems.org"
+source "https://gems.ruby-china.org"
 
 gem "jekyll"
 gem "jekyll-seo-tag"
 gem "jekyll-sitemap"
+gem "htmlbeautifier"
+gem "htmlcompressor"
 gem "jekyll-tidy"
 gem "html-proofer"
--- assets/js/bang.js
+++ assets/js/bang.js
@@ -20,6 +20,13 @@ $('#bang').click(function(){
     $("#input").focus();
 })
 
+// Append 
+$(".advanced a").click(function(){
+    h = $(this).html();
+    $(".search input").val($(".search input").val() + h); // val
+    $("#div").append(h); 
+});
+
 // Config
 var search_tag = '!';
 var default_engine = 'b';
@@ -48,11 +55,12 @@ function open_queryURL(text) {
 function get_search_engine(ss) {
     var ret = undefined;
     var arr = {
-        'ac': 'http://www.acfun.cn/search/#query=%s',
-        'am': 'https://www.amazon.com/s/?keywords=%s',
+        'a': 'https://www.amazon.com/s/?keywords=%s',
+        'ac': 'http://www.acfun.cn/search/#query=%s',        
         'aqy': 'http://so.iqiyi.com/so/q_%s',              
         'b': 'https://www.baidu.com/s?wd=%s',
         'bl': 'https://www.bilibili.com/search?keyword=%s',
+        'bm': 'https://api.map.baidu.com/geocoder?address=%s&output=html',
         'bq': 'http://www.ubiaoqing.com/search/%s',
         'bt': 'http://fanyi.baidu.com/translate#auto/zh/%s',
         'd': 'https://duckduckgo.com/?q=%s',
@@ -60,14 +68,17 @@ function get_search_engine(ss) {
         'dd': 'http://search.dangdang.com/?key=%s&enc=utf-8',       
         'g': 'http://www.google.com/search?q=%s',
         'gh': 'https://github.com/search?utf8=✓&q=%s',
+        'gm': 'https://www.google.com.hk/maps/search/%s/',
         'gt': 'https://translate.google.cn/#auto/zh-CN/%s',
         'ins': 'https://www.instagram.com/explore/tags/%s',
         'jd': 'https://search.jd.com/Search?keyword=%s&enc=utf-8',
         'kd': 'https://www.kuaidi100.com/courier/?searchText=%s',        
         'map': 'http://ditu.amap.com/search?query=%s',        
+        'pan': 'https://www.panc.cc/s/%s/',      
         'r': 'https://www.reddit.com/search?q=%s',      
         'sf': 'https://segmentfault.com/search?q=%s',      
         'so': 'http://stackoverflow.com/search?q=%s',      
+        'ss': 'https://sspai.com/search/article?q=%s',      
         'tb': 'https://s.taobao.com/search?q=%s',
         'tm': 'https://list.tmall.com/search_product.htm?q=%s',
         'tw': 'https://twitter.com/search?q=%s',
@@ -78,6 +89,7 @@ function get_search_engine(ss) {
         'yd': 'http://fanyi.youdao.com/translate?i=%s',
         'yk': 'http://www.soku.com/search_video/q_%s',
         'ytb': 'https://www.youtube.com/results?search_query=%s',
+        'zm': 'http://www.zimuzu.tv/search/index?keyword=%s',     
         'zh': 'https://www.zhihu.com/search?q=%s'     
     };
     var engine = ss.substr(1);
